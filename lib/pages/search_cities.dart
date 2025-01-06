591cdf007771c2549ba7e360338c138bafb7e6d2
Jhonproyec <albertcasti0919@gmail.com>
2025-01-06 16:21:23 -0600

    Pantalla de busqueda

--- lib/pages/search_cities.dart
+++ lib/pages/search_cities.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:material_floating_search_bar_2/material_floating_search_bar_2.dart';
 
 class SearchCities extends StatelessWidget {
   const SearchCities({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Realizar busquedas"),);
+    List cities = [];
+    FloatingSearchBarController _controller = FloatingSearchBarController();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: FloatingSearchBar(
+        controller: _controller,
+        builder: (context, index){
+          return Text("Hola mundo");
+        }
+      )
+    );
   }
 }
